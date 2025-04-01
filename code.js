e552af51aeb21ad18a95b8b771e0eb10f5383037
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-04-01 16:33:01 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,8 @@ function insertionSortReverse(array) {
     const n = array.length
     for( var i = n - 1; i >= 0; i--) { //Start at the end of the list and move to the lowest index
         var val = array[i];
-        for( var j = i; j < n && array[j+i] < val; j++) { 
+        var j;
+        for(j = i; j < n && array[j+i] < val; j++) { 
             array[j] = array[j + 1];
         }
         array[j] = val;
