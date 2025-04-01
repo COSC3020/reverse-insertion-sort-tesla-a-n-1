703b15e0e1536daad38e64a97231762259410171
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-04-01 16:32:18 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function insertionSortReverse(array) {
+    const n = array.length
+    for( var i = n - 1; i >= 0; i--) { //Start at the end of the list and move to the lowest index
+        var val = array[i];
+        for( var j = i; j < n && array[j+i] < val; j++) { 
+            array[j] = array[j + 1];
+        }
+        array[j] = val;
+    }
     return array;
 }
